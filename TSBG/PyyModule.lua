eaeec672d61520ee31902f65d90dbf5306141e71
Dumbass <129638400+scriptrblxs@users.noreply.github.com>
2025-02-01 20:30:53 +0800

    update PyyModule.lua

--- TSBG/PyyModule.lua
+++ TSBG/PyyModule.lua
@@ -178,10 +178,10 @@ function module.AnimateCamera(lifetime: number, keyframes: CFrameSequence)
 	wait(.1)
 	repeat wait() until lplr.Character ~= nil
 	workspace.CurrentCamera.CameraSubject = lplr.Character:FindFirstChildWhichIsA('Humanoid')
-	workspace.CurrentCamera.CameraType = "Custom"
+	workspace.CurrentCamera.CameraType = "Scriptable"
 	lplr.CameraMinZoomDistance = 0.5
 	lplr.CameraMaxZoomDistance = 400
-	lplr.CameraMode = "Classic"
+	lplr.CameraMode = "LockFirstPerson"
 	lplr.Character.Head.Anchored = false
 
     local origin = att.WorldCFrame
