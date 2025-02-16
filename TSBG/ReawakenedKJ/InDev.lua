5d7fe9ed64dacdc19f160849a50496cd595372f6
Dumbass <129638400+scriptrblxs@users.noreply.github.com>
2025-02-16 13:49:49 +0800

    update InDev.lua

--- TSBG/ReawakenedKJ/InDev.lua
+++ TSBG/ReawakenedKJ/InDev.lua
@@ -161,6 +161,7 @@ local ivfx2 = game.ReplicatedStorage.Resources.KJEffects["fine...1OG"]["REDDDD1"
 local ivfx3 = game.ReplicatedStorage.Resources.KJEffects["fine...1OG"]["REDDDD2"]:Clone() ivfx.Parent = char.HumanoidRootPart
 local ivfx4 = game.ReplicatedStorage.Resources.KJEffects["fine...1OG"]["REDDDD3"]:Clone() ivfx.Parent = char["Left Arm"]
 local ivfx5 = game.ReplicatedStorage.Resources.KJEffects["fine...1OG"]["REDDDD4"]:Clone() ivfx.Parent = char["Right Arm"]
+local endvfx = game.ReplicatedStorage.Resources.KJEffects["stoic bomb boom "]["THEACTUALBOOM"]:Clone() endvfx.Parent = char.HumanoidRootPart
 local function awakeningVFXBatch2(duration:number)
     for _, v in pairs(ivfx1:GetDescendants()) do
         if v:IsA("ParticleEmitter") then
@@ -182,6 +183,7 @@ local function awakeningVFXBatch2(duration:number)
         ivfx3.Enabled = false
         ivfx4.Enabled = false
         ivfx5.Enabled = false
+        endvfx:En
     end)
 end
 
