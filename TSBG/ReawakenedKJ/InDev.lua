0754c9b2551bceccfce8dc37582965c0997ff036
Dumbass <129638400+scriptrblxs@users.noreply.github.com>
2025-03-21 16:20:07 +0800

    update InDev.lua

--- TSBG/ReawakenedKJ/InDev.lua
+++ TSBG/ReawakenedKJ/InDev.lua
@@ -249,12 +249,12 @@ local function emitParticles(attachment: Attachment)
     end
 end
 
-local function enableParticles(attachment: Attachment)
+local function enableParticles(attachment: Attachment, duration: number)
     assert(attachment:IsA("Attachment"), "Particle container must be attachment!")
     for _, v in pairs(attachment:GetDescendants()) do
         if v:IsA("Attachment") then
             v.Enabled = true
-            task.wait(v:GetAttribute("EmitDuration") or 1, function()
+            task.delay(duration or v:GetAttribute("EmitDuration") or 1, function()
                 v.Enabled = false
             end)
         end
